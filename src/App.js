d4e3c4e0f7b71f37bd2cb3ee031da37e8b274349
nikosNtertilis <ntertilis21@gmail.com>
2021-03-31 21:53:25 +0300

    todolist

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import TodosList from "./components/todos";
-import { TodosProvider } from "./components/todosContext";
-
 
 function App() {
   
   return (
-    <TodosProvider>
-      <TodosList />
-    </TodosProvider>
-    
+    <div>
+      <TodosList /> 
+    </div>
   );
 }
 
--- src/components/todos/todosList.js
+++ src/components/todos/todosList.js
@@ -1,12 +1,31 @@
-import React, { useContext } from 'react'
+import React, { useState, useEffect } from 'react'
 import { todosContext } from '../todosContext';
 
 const TodosList =() => {
-    const { state:{ todosArr } } = useContext(todosContext);
-    console.log(todosArr);
+    // const { state:{ todosArr } } = useContext(todosContext);
+    // console.log(todosArr);
+
+    const [todos, setTodos] = useState([])
+
+    useEffect(() => {
+        const fetchTodos = async () => {
+            const inputText= 1;
+            const response = await fetch(
+                `https://jsonplaceholder.typicode.com/todos?${inputText}`
+                );
+                const json = await response.json();
+                setTodos([...json]);
+        }
+
+        fetchTodos();
+    }, [todos])
+
     return (
         <div>
-            
+            {todos.map(item => 
+            <div key={item.id}>
+                {item.title}
+            </div> )}
         </div>
     )
 }
