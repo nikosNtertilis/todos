fe7f9ca8bbb9a715e242fe2433586fcb4b401562
nikosNtertilis <ntertilis21@gmail.com>
2021-03-31 21:54:36 +0300

    cleanup

--- src/components/todos/todosList.js
+++ src/components/todos/todosList.js
@@ -1,10 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { todosContext } from '../todosContext';
 
 const TodosList =() => {
-    // const { state:{ todosArr } } = useContext(todosContext);
-    // console.log(todosArr);
-
     const [todos, setTodos] = useState([])
 
     useEffect(() => {
@@ -13,8 +9,9 @@ const TodosList =() => {
             const response = await fetch(
                 `https://jsonplaceholder.typicode.com/todos?${inputText}`
                 );
-                const json = await response.json();
-                setTodos([...json]);
+            const json = await response.json();
+            
+            setTodos([...json]);
         }
 
         fetchTodos();
