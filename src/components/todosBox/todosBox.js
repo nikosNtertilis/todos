e899ead0894c269a86106155fb5cc924e97d221a
nikosNtertilis <ntertilis21@gmail.com>
2021-03-31 23:22:01 +0300

    completed addon

--- src/components/todosBox/todosBox.js
+++ src/components/todosBox/todosBox.js
@@ -2,9 +2,24 @@ import React from 'react'
 import './todosBox.css'
 
 const TodosBox = ({item}) => {
+
+    const todoCompleted = 
+    item.completed
+    ? "not completed"
+    : "completed"
+    
+    const ifcompleted = 
+    item.completed
+    ? "not-completed"
+    : "completed"
     return (
         <div className="todosBox" id={item.id}>
-            {item.title}
+            <div>
+                {item.title}
+            </div>
+            <div className={`${ifcompleted} align-bottom`}>
+                {todoCompleted}
+            </div>
         </div>
     )
 }
