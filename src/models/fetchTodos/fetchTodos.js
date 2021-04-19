9c91ecec1a801369bb5bf7e0acff317c0c7ae51f
nikosNtertilis <ntertilis21@gmail.com>
2021-04-19 23:50:27 +0300

    install redux

--- src/models/fetchTodos/fetchTodos.js
+++ src/models/fetchTodos/fetchTodos.js
@@ -1,6 +1,5 @@
 const urlJsonPlaceHolder = `https://jsonplaceholder.typicode.com/todos` ;
 
-
 const fetchTodos = async () => {
     const response = await fetch(urlJsonPlaceHolder);
     const json = await response.json();
